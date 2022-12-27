e2a3f4ff91d2e6973fc31d2135d8d62da706ab6c
NicolayBelyaev <nickb2147@gmail.com>
2022-12-27 21:35:29 +0300

    seminar 03_homework_fullname parser_minor fix

--- src/main/java/homework03/Main.java
+++ src/main/java/homework03/Main.java
@@ -2,7 +2,7 @@ package homework03;
 
 public class Main {
     public static void main(String[] args) {
-        String inStr = "Belyaev Nikolay Sergeevich 30.12.1991 9215151502 m";
+        String inStr = "Belyaev Nick Serg 30.12.1991 9215151502 m";
         StringParser strPars = new StringParser(inStr);
         System.out.println(strPars.getSurname());
         System.out.println(strPars.getName());
--- src/main/java/homework03/StringParser.java
+++ src/main/java/homework03/StringParser.java
@@ -3,7 +3,7 @@ package homework03;
 import java.util.Date;
 
 public class StringParser {
-    public  StringBuilder fullName = new StringBuilder();
+    private final StringBuilder fullName = new StringBuilder();
     private String surname;
     private String name;
     private String patronymic;
@@ -16,11 +16,17 @@ public class StringParser {
         try {
             parsFullName(sbIncomingString);
             surname = dismemberFullName();
-            name = dismemberFullName();
-            patronymic = fullName.substring(0, fullName.length() - 1).toString();
         } catch (IndexOutOfBoundsException e) {
             System.out.println("Имя не указано/указано неверно или нарушен формат ввода. " +
-                               "Начните ввод данных c имя (+ фамилия и отчество если есть).");
+                               "Начните ввод данных c имени (+ фамилия и отчество если есть).");
+        }
+        if (!fullName.isEmpty()) {
+            name = dismemberFullName();
+        }
+        if (!fullName.isEmpty()) {
+            patronymic = fullName.charAt(fullName.length() - 1) == ' ' ?
+                    fullName.substring(0, fullName.length() - 1) :
+                    fullName.toString();
         }
         parsBirthday();
         parsPhoneNumber();
@@ -28,23 +34,22 @@ public class StringParser {
     }
 
     private void parsFullName(StringBuilder sbIncomingstring) {
-        int charIndex = 0;
-        while(!Character.isDigit(sbIncomingstring.charAt(charIndex))) {
-            fullName.append(sbIncomingstring.charAt(charIndex));
-            charIndex++;
+        while(!Character.isDigit(sbIncomingstring.charAt(0))) {
+            fullName.append(sbIncomingstring.charAt(0));
+            sbIncomingstring.deleteCharAt(0);
         }
         while (fullName.charAt(0) == ' ') {
             fullName.deleteCharAt(0);
         }
-        sbIncomingstring.delete(0, fullName.length());
     }
     private String dismemberFullName() {
         StringBuilder memberOfFullName = new StringBuilder();
-        while(!Character.isWhitespace(fullName.charAt(0))) {
+        while(fullName.length() != 0 && !Character.isWhitespace(fullName.charAt(0))) {
             memberOfFullName.append(fullName.charAt(0));
             fullName.deleteCharAt(0);
         }
-        while (fullName.charAt(0) == ' ') {
+
+        while (fullName.length() != 0 && fullName.charAt(0) == ' ') {
             fullName.deleteCharAt(0);
         }
         return memberOfFullName.toString();
@@ -59,8 +64,6 @@ public class StringParser {
     private void parsSex() {
 
     }
-
-
     public String getSurname() {return surname;}
     public String getName() {return name;}
     public String getPatronymic() {return patronymic;}
